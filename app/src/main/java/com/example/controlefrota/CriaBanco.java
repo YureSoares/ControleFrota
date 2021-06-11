822162b1a9c175c0397ee5dd1544c6045822c86a
Marcelo jr <desenvolvimento1@arcus.com.br>
2021-06-11 17:44:41 -0300

    Começo da implatanção do Banco de Dados

---
+++ app/src/main/java/com/example/controlefrota/CriaBanco.java
@@ -0,0 +1,24 @@
+package com.example.controlefrota;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class CriaBanco extends SQLiteOpenHelper {
+
+    public CriaBanco(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
